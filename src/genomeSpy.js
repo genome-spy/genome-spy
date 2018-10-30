39c215c34e6ee981e4dbbf6f18be92185d882394
Kari Lavikka <kari.lavikka@bdb.fi>
2018-10-30 18:26:55 +0200

    Fixes an offset problem with zooming

--- src/genomeSpy.js
+++ src/genomeSpy.js
@@ -64,38 +64,62 @@ export default class GenomeSpy {
             viewport: new Interval(aw, aw + viewportWidth)
         };
 
+        this.viewportOverlay.style.left = `${aw}px`;
+        this.viewportOverlay.style.width = `${viewportWidth}px`;
+
         this.eventEmitter.emit('layout', layout);
     }
 
     // TODO: Come up with a sensible name. And maybe this should be called at the end of the constructor.
     launch() {
-        const spy = this;
-
         window.addEventListener('resize', this._resized.bind(this), false);
+
+        this.container.classname = "genome-spy";
+
+        const trackStack = document.createElement("div");
+        trackStack.className = "track-stack";
+        // TODO: Put these to CSS
+        trackStack.style.position = "absolute";
+        trackStack.style.top = 0;
+        trackStack.style.bottom = 0;
+        trackStack.style.left = 0;
+        trackStack.style.right = 0;
+        trackStack.style.display = "flex";
+        trackStack.style.flexDirection = "column";
+
+        this.tracks.forEach(track => {
+            const trackContainer = document.createElement("div");
+            trackContainer.className = "genome-spy-track";
+            trackStack.appendChild(trackContainer);
+
+            track.initialize({genomeSpy: this, trackContainer});
+        });
+
+        this.container.appendChild(trackStack);
+
+        const viewportOverlay = document.createElement("div");
+        viewportOverlay.className = "viewport-overlay";
+        // TODO: Put these to CSS
+        viewportOverlay.style.position = "absolute";
+        viewportOverlay.style.top = 0;
+        viewportOverlay.style.bottom = 0;
+
+        this.container.appendChild(viewportOverlay);
 
         const genomeExtent = this.chromMapper.extent();
 
-        d3.select(spy.container).call(this.zoom
+        d3.select(viewportOverlay).call(this.zoom
             /* // Borken!
             .extent([
                 [this.getAxisWidth(), 0],
                 [this.container.offsetWidth, this.container.offsetHeight]])
                 */
-            .scaleExtent([1, genomeExtent[1] / spy.container.offsetWidth / 10])
+            .scaleExtent([1, genomeExtent[1] / this.container.offsetWidth / 10])
             .translateExtent([[genomeExtent[0], -Infinity], [genomeExtent[1], Infinity]]) // Check this: https://bl.ocks.org/mbostock/4015254
             .on("zoom", this._zoomed.bind(this)));
 
-        this.container.styleClass = "genome-spy";
-        this.container.style.display = "flex"; // TODO: CSS
-        this.container.style.flexDirection = "column";
-
-        spy.tracks.forEach(track => {
-            const trackContainer = document.createElement("div");
-            trackContainer.className = "genome-spy-track";
-            this.container.appendChild(trackContainer);
-
-            track.initialize({genomeSpy: this, trackContainer});
-        });
+        this.trackStack = trackStack;
+        this.viewportOverlay = viewportOverlay;
 
         this._resized();
     }
