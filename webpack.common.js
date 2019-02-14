0b13c1cbe81c10976f3f075f404c0195a6ff164c
Kari Lavikka <kari.lavikka@bdb.fi>
2019-02-14 23:18:19 +0200

    Fixes missing css in production mode production build

--- webpack.common.js
+++ webpack.common.js
@@ -1,18 +1,14 @@
 const {resolve} = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const WebpackMd5Hash = require('webpack-md5-hash');
 const path = require('path');
 
 module.exports = {
 
   plugins: [
-    new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       title: 'GenomeSpy GL',
       hash: true
-    }),
-    new WebpackMd5Hash()
+    })
   ],
 
   output: {
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,24 +1,26 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(common, {
     plugins: [
+        new CleanWebpackPlugin(['dist']),
         new MiniCssExtractPlugin({
             filename: '[name].css',
         }),
-        new HtmlWebpackPlugin({
+        //new HtmlWebpackPlugin({
         //    inlineSource: '.(js|css)$' // embed all javascript and css inline
-        }),
+        //}),
         //new HtmlWebpackInlineSourcePlugin()
     ],
 
     module: {
         rules: [
             {
-                test: /\.(s*)css$/,
+                test: /\.(s?)css$/,
                 use: [
                     MiniCssExtractPlugin.loader,
                     'css-loader',
