c584bd85fe57cbb5ca985646caadcf1c4de28049
Kari Lavikka <kari.lavikka@helsinki.fi>
2019-04-12 13:29:03 +0300

    Adjusts precisions in poit fragment shader

--- src/gl/point.fragment.glsl
+++ src/gl/point.fragment.glsl
@@ -1,11 +1,11 @@
 precision mediump float;
 
-const vec4 white = vec4(vec3(1), 1);
-const vec4 black = vec4(vec3(0), 1);
+const lowp vec4 white = vec4(vec3(1), 1);
+const lowp vec4 black = vec4(vec3(0), 1);
 
-uniform float devicePixelRatio;
+uniform lowp float devicePixelRatio;
 
-varying vec4 vColor;
+varying lowp vec4 vColor;
 varying float vSize;
 
 // TODO: Implement more symbols: diamond, triangle, etc
@@ -17,16 +17,16 @@ void main() {
     if (dist > 0.5)
         discard;
 
-    vec4 strokeColor = mix(vColor, black, 0.3); 
+    lowp vec4 strokeColor = mix(vColor, black, 0.3); 
     // Stuble radial gradient
-    vec4 fillColor = mix(vColor, white, 0.25 - dist * 0.5);
+    lowp vec4 fillColor = mix(vColor, white, 0.25 - dist * 0.5);
 
     // Could use fwidth here, but GL_OES_standard_derivatives is not always available
     float pixelWidth = 1.0 / vSize;
     float strokeWidth = 0.7 * devicePixelRatio * pixelWidth;
     
-    float strokeFraction = smoothstep(0.5 - strokeWidth, 0.5 - strokeWidth - pixelWidth, dist);
-    float alpha = smoothstep(0.5, 0.5 - pixelWidth, dist);
+    lowp float strokeFraction = smoothstep(0.5 - strokeWidth, 0.5 - strokeWidth - pixelWidth, dist);
+    lowp float alpha = smoothstep(0.5, 0.5 - pixelWidth, dist);
 
     gl_FragColor = mix(strokeColor, fillColor, strokeFraction) * alpha;
 }
