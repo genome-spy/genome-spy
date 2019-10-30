5c5fa5013ee1002d4398d7e3412c1191c6612822
Kari Lavikka <kari.lavikka@helsinki.fi>
2019-10-30 14:24:36 +0200

    Lerna monorepo

--- packages/genome-spy-doc-embed/webpack.config.js
+++ packages/genome-spy-doc-embed/webpack.config.js
@@ -1,37 +1,43 @@
-const merge = require('webpack-merge');
-const common = require('./webpack.common.js');
+//const merge = require('webpack-merge');
+//const common = require('./webpack.common.js');
 const path = require('path');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+//const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = merge(common, {
+module.exports = {
     mode: 'production',
     devtool: 'source-map',
     
     entry: {
-        main: './src/docEmbed.js'
+        main: './index.js'
     },
     
     plugins: [
+/*
         new MiniCssExtractPlugin({
             filename: '[name].css',
         })
+*/
     ],
 
+/*
     output: {
         filename: '[name].js',
-        path: path.resolve(__dirname, 'docs/app/')
+        path: path.resolve(__dirname, '../docs/app/')
     },
+*/
 
     module: {
         rules: [
             {
                 test: /\.(s*)css$/,
                 use: [
+/*
                     'style-loader',
                     'css-loader',
                     'sass-loader'
+*/
                 ]
             }
         ]
     }
-});
+};
