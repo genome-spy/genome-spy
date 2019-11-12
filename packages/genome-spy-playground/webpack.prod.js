19874f76c51bd369fa96146258990e62b5f05dcf
Kari Lavikka <kari.lavikka@helsinki.fi>
2019-11-12 11:48:20 +0200

    chore: CleanWebpackPlugin: dist/ of playground

--- packages/genome-spy-playground/webpack.prod.js
+++ packages/genome-spy-playground/webpack.prod.js
@@ -1,45 +1,43 @@
-const merge = require('webpack-merge');
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require("webpack-merge");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    mode: 'production',
-    devtool: 'source-map',
-    
+    mode: "production",
+    devtool: "source-map",
+
     entry: {
-        main: './src/index.js'
+        main: "./src/index.js"
     },
-    
+
     plugins: [
+        new CleanWebpackPlugin(["dist"]),
         new HtmlWebpackPlugin({
-            title: 'GenomeSpy Playground',
+            title: "GenomeSpy Playground",
             hash: true
         }),
         new MiniCssExtractPlugin()
     ],
 
     output: {
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, "dist")
     },
 
     module: {
         rules: [
             {
                 test: /\.(s*)css$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'sass-loader'
-                ]
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
             },
             {
                 test: /\.glsl$/,
-                use: 'webpack-glsl-loader'
+                use: "webpack-glsl-loader"
             },
             {
                 test: /\.(txt|[ct]sv)$/,
-                use: 'raw-loader'
+                use: "raw-loader"
             }
         ]
     }
