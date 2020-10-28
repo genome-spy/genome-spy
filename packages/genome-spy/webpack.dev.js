4d2acd82a3ecac3c4295f2b6e8178ef64b76c538
Kari Lavikka <kari.lavikka@helsinki.fi>
2020-10-28 15:53:03 +0200

    use eval-cheap-source-map in development

--- packages/genome-spy/webpack.dev.js
+++ packages/genome-spy/webpack.dev.js
@@ -1,35 +1,31 @@
-const merge = require('webpack-merge');
-const common = require('./webpack.common.js');
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require("webpack-merge");
+const common = require("./webpack.common.js");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = merge(common, {
-    mode: 'development',
-    devtool: 'inline-source-map',
-    
+    mode: "development",
+    devtool: "eval-cheap-source-map",
+
     entry: {
-        main: './src/singlePageApp.js'
+        main: "./src/singlePageApp.js"
     },
-    
+
     plugins: [
         new HtmlWebpackPlugin({
-            title: 'GenomeSpy'
+            title: "GenomeSpy"
         })
     ],
 
     devServer: {
-        contentBase: path.join(__dirname, 'static')
+        contentBase: path.join(__dirname, "static")
     },
 
     module: {
         rules: [
             {
                 test: /\.(s*)css$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
+                use: ["style-loader", "css-loader", "sass-loader"]
             }
         ]
     }
