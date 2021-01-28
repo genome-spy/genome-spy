f9fea05fff87a03b77e93bb62dd528096fbbebf3
Kari Lavikka <kari.lavikka@helsinki.fi>
2021-01-28 18:15:02 +0200

    fix connection mark's edge aa

--- packages/genome-spy/src/gl/connection.fragment.glsl
+++ packages/genome-spy/src/gl/connection.fragment.glsl
@@ -8,7 +8,7 @@ void main(void) {
     float dpr = uDevicePixelRatio;
 
     float distance = abs(vNormalLengthInPixels);
-    float opacity = clamp(((vSize / 2.0 - distance) * dpr), -0.5, 0.5) + 0.5;
+    float opacity = clamp(((vSize / 2.0 - distance) * dpr), 0.0, 1.0);
 
     fragColor = vColor * opacity;
 }
