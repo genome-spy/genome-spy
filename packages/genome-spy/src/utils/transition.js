ddd39eeb240737cd5611bdefaeaf052ecb72f6ca
Kari Lavikka <kari.lavikka@helsinki.fi>
2021-08-25 18:07:50 +0300

    feat: abortable transition

--- packages/genome-spy/src/utils/transition.js
+++ packages/genome-spy/src/utils/transition.js
@@ -1,7 +1,4 @@
 /**
- *
- * TODO: Implement AbortController: https://developer.mozilla.org/en-US/docs/Web/API/AbortController
- *
  * @param {TransitionOptions} options
  *
  * @typedef {Object} TransitionOptions
@@ -12,38 +9,48 @@
  * @prop {function(number):number} [easingFunction] default: linear
  * @prop {function(function(number):void):void} [requestAnimationFrame]
  *      default: window.requestAnimationFrame
+ * @prop {AbortSignal} [signal]
  */
 export default function transition(options) {
     const requestAnimationFrame =
         options.requestAnimationFrame || window.requestAnimationFrame;
 
-    return new Promise(resolve => {
+    const signal = options.signal;
+
+    if (signal?.aborted) {
+        return Promise.reject("aborted");
+    }
+
+    return new Promise((resolve, reject) => {
         const beginTimestamp = performance.now();
         const endTimestamp = beginTimestamp + (options.duration || 1000);
 
         const from = typeof options.from == "number" ? options.from : 0;
         const to = typeof options.to == "number" ? options.to : 1;
-        const ease = options.easingFunction || (x => x);
+        const ease = options.easingFunction || ((x) => x);
 
         /** @param {number} x */
-        const toUnit = x =>
+        const toUnit = (x) =>
             (x - beginTimestamp) / (endTimestamp - beginTimestamp);
 
         /** @param {number} x */
-        const toRange = x => x * (to - from) + from;
+        const toRange = (x) => x * (to - from) + from;
 
         /** @param {number} x */
-        const clamp = x => Math.max(0, Math.min(1, x));
+        const clamp = (x) => Math.max(0, Math.min(1, x));
 
         /** @param {number} stamp */
-        const step = stamp => {
-            options.onUpdate(toRange(ease(clamp(toUnit(stamp)))));
-
-            if (stamp < endTimestamp) {
-                requestAnimationFrame(step);
+        const step = (stamp) => {
+            if (signal?.aborted) {
+                reject("aborted");
             } else {
-                options.onUpdate(toRange(ease(1)));
-                resolve();
+                options.onUpdate(toRange(ease(clamp(toUnit(stamp)))));
+                if (stamp < endTimestamp) {
+                    requestAnimationFrame(step);
+                } else {
+                    options.onUpdate(toRange(ease(1)));
+                    resolve();
+                }
             }
         };
 
