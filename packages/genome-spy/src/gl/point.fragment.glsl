c2fa382600805c3d3e7e112c8c3839be16afe4ba
Kari Lavikka <kari.lavikka@helsinki.fi>
2021-09-13 08:56:55 +0300

    fix: sharp corners to diamond

--- packages/genome-spy/src/gl/point.fragment.glsl
+++ packages/genome-spy/src/gl/point.fragment.glsl
@@ -63,17 +63,10 @@ float crossShape() {
     return min(max(v.x, v.y), max(h.x, h.y));
 }
 
-float ndot(vec2 a, vec2 b) {
-    return a.x * b.x - a.y * b.y;
-}
-
-float sdRhombus() {
-    const vec2 b = vec2(1.0);
-
-    vec2 q = abs(npc());
-    float h = clamp((-2.0 * ndot(q, b) + ndot(b, b)) / dot(b, b), -1.0, 1.0);
-    float d = length(q - 0.5 * b * vec2(1.0 - h, 1.0 + h));
-    return d * sign(q.x * b.y + q.y * b.x - b.x * b.y);
+float diamond() {
+	float r = 1.0;
+    vec2 p = abs(npc());
+    return (max(abs(p.x - p.y), abs(p.x + p.y)) - r) / sqrt(2.0);
 }
 
 void main() {
@@ -93,7 +86,7 @@ void main() {
         dist = crossShape();
 
     } else if (vShape == DIAMOND) {
-        dist = sdRhombus();
+        dist = diamond();
 
     } else if (vShape == TRIANGLE_DOWN) {
         dist = equilateralTriangle(false, false);
