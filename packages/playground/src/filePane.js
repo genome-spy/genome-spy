606e47783b506753579d0f77766406139e82f900
Kari Lavikka <kari.lavikka@helsinki.fi>
2021-11-25 15:43:50 +0200

    refactor(playground): put file upload pane into a web component
    Makes `index.js`much tidier

---
+++ packages/playground/src/filePane.js
@@ -0,0 +1,243 @@
+import { html, LitElement } from "lit";
+import { read } from "vega-loader";
+
+/**
+ * @typedef {Record<string, any>} Datum
+ * @typedef {{ metadata: File, data: any }} FileEntry
+ */
+
+/**
+ *
+ */
+export default class FilePane extends LitElement {
+    /** @type {string | undefined} */
+    _currentTab;
+
+    /** @type {Record<string, FileEntry>} */
+    files;
+
+    constructor() {
+        super();
+    }
+
+    createRenderRoot() {
+        // No shadow DOM, please. Styles don't get through.
+        return this;
+    }
+
+    render() {
+        return html`
+            <ul class="tabs">
+                ${Object.keys(this.files).map(
+                    (name) =>
+                        html`
+                            <li
+                                data-name=${name}
+                                class=${name == this._currentTab
+                                    ? "selected"
+                                    : ""}
+                            >
+                                <a
+                                    href="#"
+                                    @click=${(/** @type {UIEvent} */ event) =>
+                                        this._changeTab(event)}
+                                    >${name}</a
+                                >
+                            </li>
+                        `
+                )}
+                <li class=${this._currentTab === undefined ? "selected" : ""}>
+                    <a
+                        href="#"
+                        @click=${(/** @type {UIEvent} */ event) =>
+                            this._changeTab(event)}
+                        >Add new files</a
+                    >
+                </li>
+                <li style="flex-grow: 1"></li>
+            </ul>
+
+            <div class="tab-pages">
+                ${Object.keys(this.files).map(
+                    (name) => html`
+                        <div
+                            class=${name == this._currentTab ? "selected" : ""}
+                        >
+                            ${makeDataTable(this.files[name].data)}
+                        </div>
+                    `
+                )}
+
+                <div class=${this._currentTab === undefined ? "selected" : ""}>
+                    ${makeUploadForm((event) => this._handleFiles(event))}
+                </div>
+            </div>
+        `;
+    }
+
+    /**
+     *
+     * @param {InputEvent} event
+     */
+    async _handleFiles(event) {
+        const target = /** @type {HTMLInputElement} */ (event.target);
+        const fileList = target.files;
+
+        for (const file of fileList) {
+            const textContent = await readFileAsync(file);
+
+            const data = read(textContent, {
+                type: inferFileType(textContent, file.name),
+                parse: "auto",
+            });
+
+            this.files[file.name] = {
+                metadata: file,
+                data,
+            };
+
+            this._currentTab = file.name;
+        }
+
+        this.requestUpdate();
+        this.dispatchEvent(new CustomEvent("upload", { detail: {} }));
+    }
+
+    /**
+     * @param {UIEvent} event
+     */
+    _changeTab(event) {
+        const target = /** @type {HTMLElement} */ (event.target);
+        const name = target.parentElement.dataset.name;
+        this._currentTab = name;
+
+        event.preventDefault();
+        this.requestUpdate();
+    }
+}
+
+customElements.define("file-pane", FilePane);
+
+// Utils
+
+/**
+ *
+ * @param {(event: InputEvent) => void} handleFiles
+ * @returns
+ */
+function makeUploadForm(handleFiles) {
+    return html` <form class="upload-form">
+        <input
+            type="file"
+            multiple
+            accept=".csv,.tsv,.txt,.json"
+            id="fileInput"
+            @change=${handleFiles}
+            style="display:none"
+        />
+        <div id="upload-button-wrapper">
+            <button
+                class="btn"
+                @click=${(/** @type {UIEvent} */ e) => {
+                    document.getElementById("fileInput").click();
+                    e.preventDefault();
+                    e.stopPropagation();
+                }}
+            >
+                Choose files
+            </button>
+        </div>
+
+        <p>
+            The added file becomes a named datasource, which can be accessed as
+            follows:
+        </p>
+
+        <pre>
+"data": {
+    "name": "filename.csv"
+}
+</pre
+        >
+
+        <p>
+            N.B. All data processing takes place in your web browser. Nothing is
+            uploaded anywhere.
+        </p>
+    </form>`;
+}
+
+/**
+ *
+ * @param {Datum[]} data
+ */
+function makeDataTable(data) {
+    const cols = Object.keys(data[0]);
+    const rows = data.slice(0, 30);
+
+    const alignments = cols.map(
+        (col) =>
+            "text-align: " +
+            (typeof data[0][col] === "number" ? "right" : "left")
+    );
+
+    const makeRow = (/** @type {Datum[]} */ row) => html`
+        <tr>
+            ${cols.map(
+                (c, i) => html`<td style=${alignments[i]}>${row[c]}</td> `
+            )}
+        </tr>
+    `;
+
+    const makeEllipsis = () => html`
+        <tr>
+            ${cols.map((c, i) => html`<td style=${alignments[i]}>...</td> `)}
+        </tr>
+    `;
+
+    const makeHead = () => html`
+        <tr>
+            ${cols.map((c, i) => html`<th style=${alignments[i]}>${c}</th> `)}
+        </tr>
+    `;
+
+    return html`
+        <table class="data-sample-table">
+            <thead>
+                ${makeHead()}
+            </thead>
+            <tbody>
+                ${rows.map(makeRow)}
+                ${rows.length < data.length ? makeEllipsis() : ``}
+            </tbody>
+        </table>
+    `;
+}
+
+/**
+ * https://simon-schraeder.de/posts/filereader-async/
+ *
+ * @param {File} file
+ * @returns {Promise<string>}
+ */
+function readFileAsync(file) {
+    return new Promise((resolve, reject) => {
+        let reader = new FileReader();
+        reader.onload = () => resolve(/** @type {string} */ (reader.result));
+        reader.onerror = reject;
+        reader.readAsText(file);
+    });
+}
+
+/**
+ * @param {string} contents
+ * @param {string} name
+ */
+function inferFileType(contents, name) {
+    if (/\.json$/.test(name)) {
+        return "json";
+    } else {
+        // In bioinformatics, csv files are often actually tsv files
+        return contents.indexOf("\t") >= 0 ? "tsv" : "csv";
+    }
+}
--- packages/playground/src/index.js
+++ packages/playground/src/index.js
@@ -3,11 +3,11 @@ import { html, render } from "lit";
 import { ref, createRef } from "lit/directives/ref.js";
 import { icon } from "@fortawesome/fontawesome-svg-core";
 import { faColumns, faQuestionCircle } from "@fortawesome/free-solid-svg-icons";
-import { read } from "vega-loader";
 import { embed, icon as genomeSpyIcon } from "@genome-spy/core";
 import { debounce } from "@genome-spy/core/utils/debounce";
 import defaultSpec from "./defaultspec.json.txt";
 import "./codeEditor";
+import "./filePane";
 import "./playground.scss";
 
 const STORAGE_KEY = "playgroundSpec";
@@ -16,6 +16,9 @@ const genomeSpyContainerRef = createRef();
 
 /** @type {import("lit/directives/ref.js").Ref<import("./codeEditor").default>} */
 const editorRef = createRef();
+
+/** @type {Record<string, import("./filePane").FileEntry>} */
+const files = {};
 
 /** @type {any} TODO: Proper type */
 let embedResult;
@@ -26,12 +29,6 @@ let previousStringifiedSpec = "";
 
 const layouts = ["stacked", "parallel", "full"];
 let layout = layouts[0];
-
-/** @type {Record<string, any>} */
-const files = {};
-
-/** @type {string} */
-let currentTab;
 
 function toggleLayout() {
     layout = layouts[(layouts.indexOf(layout) + 1) % layouts.length];
@@ -79,69 +76,6 @@ async function update(force = false) {
     }
 }
 
-/**
- * https://simon-schraeder.de/posts/filereader-async/
- *
- * @param {File} file
- * @returns {Promise<string>}
- */
-function readFileAsync(file) {
-    return new Promise((resolve, reject) => {
-        let reader = new FileReader();
-        reader.onload = () => resolve(/** @type {string} */ (reader.result));
-        reader.onerror = reject;
-        reader.readAsText(file);
-    });
-}
-
-/**
- * @param {string} contents
- * @param {string} name
- */
-function inferFileType(contents, name) {
-    if (/\.json$/.test(name)) {
-        return "json";
-    } else {
-        // In bioinformatics, csv files are often actually tsv files
-        return contents.indexOf("\t") >= 0 ? "tsv" : "csv";
-    }
-}
-
-async function handleFiles(event) {
-    const fileList = event.target.files;
-
-    for (const file of fileList) {
-        const textContent = await readFileAsync(file);
-
-        const data = read(textContent, {
-            type: inferFileType(textContent, file.name),
-            parse: "auto",
-        });
-
-        files[file.name] = {
-            metadata: file,
-            data,
-        };
-
-        currentTab = file.name;
-    }
-
-    renderLayout();
-    update(true);
-}
-
-/**
- * @param {UIEvent} event
- */
-function changeTab(event) {
-    const target = /** @type {HTMLElement} */ (event.target);
-    const name = target.parentElement.dataset.name;
-    currentTab = name;
-
-    event.preventDefault();
-    renderLayout();
-}
-
 const toolbarTemplate = () => html`
     <div class="toolbar">
         <img
@@ -164,129 +98,6 @@ const toolbarTemplate = () => html`
     </div>
 `;
 
-const singleFileTemplate = (f) => {
-    const cols = Object.keys(f.data[0]);
-    const rows = f.data.slice(0, 30);
-
-    const alignments = cols.map(
-        (col) =>
-            "text-align: " +
-            (typeof f.data[0][col] === "number" ? "right" : "left")
-    );
-
-    return html`
-        <table class="data-sample-table">
-            <thead>
-                <tr>
-                    ${cols.map(
-                        (c, i) => html` <th style=${alignments[i]}>${c}</th> `
-                    )}
-                </tr>
-            </thead>
-            <tbody>
-                ${rows.map(
-                    (row) => html`
-                        <tr>
-                            ${cols.map(
-                                (c, i) =>
-                                    html`
-                                        <td style=${alignments[i]}>
-                                            ${row[c]}
-                                        </td>
-                                    `
-                            )}
-                        </tr>
-                    `
-                )}
-                ${rows.length < f.data.length
-                    ? html`
-                          <tr>
-                              ${cols.map(
-                                  (c, i) =>
-                                      html`
-                                          <td style=${alignments[i]}>...</td>
-                                      `
-                              )}
-                          </tr>
-                      `
-                    : ``}
-            </tbody>
-        </table>
-    `;
-};
-
-const fileTemplate = () => html`
-    <ul class="tabs">
-        ${Object.keys(files).map(
-            (name) =>
-                html`
-                    <li
-                        data-name=${name}
-                        class=${name == currentTab ? "selected" : ""}
-                    >
-                        <a href="#" @click=${changeTab}>${name}</a>
-                    </li>
-                `
-        )}
-        <li class=${currentTab === undefined ? "selected" : ""}>
-            <a href="#" @click=${changeTab}>Add new files</a>
-        </li>
-        <li style="flex-grow: 1"></li>
-    </ul>
-
-    <div class="tab-pages">
-        ${Object.keys(files).map(
-            (name) => html`
-                <div class=${name == currentTab ? "selected" : ""}>
-                    ${singleFileTemplate(files[name])}
-                </div>
-            `
-        )}
-
-        <div class=${currentTab === undefined ? "selected" : ""}>
-            <form class="upload-form">
-                <input
-                    type="file"
-                    multiple
-                    accept=".csv,.tsv,.txt,.json"
-                    id="fileInput"
-                    @change=${handleFiles}
-                    style="display:none"
-                />
-                <div id="upload-button-wrapper">
-                    <button
-                        class="btn"
-                        @click=${(/** @type {UIEvent} */ e) => {
-                            document.getElementById("fileInput").click();
-                            e.preventDefault();
-                            e.stopPropagation();
-                        }}
-                    >
-                        Choose files
-                    </button>
-                </div>
-
-                <p>
-                    The added file becomes a named datasource, which can be
-                    accessed as follows:
-                </p>
-
-                <pre>
-"data": {
-    "name": "filename.csv"
-}
-</pre
-                >
-
-                <p>
-                    N.B. All data processing takes place in your web browser.
-                    Nothing is uploaded anywhere.
-                </p>
-            </form>
-        </div>
-    </div>
-`;
-
 const debouncedUpdate = debounce(() => update(), 500, false);
 
 const layoutTemplate = () => html`
@@ -302,7 +113,9 @@ const layoutTemplate = () => html`
         <section id="genome-spy-pane">
             <div ${ref(genomeSpyContainerRef)}></div>
         </section>
-        <section id="file-pane">${fileTemplate()}</section>
+        <section id="file-pane">
+            <file-pane @upload=${update} .files=${files}></file-pane>
+        </section>
     </section>
 `;
 
