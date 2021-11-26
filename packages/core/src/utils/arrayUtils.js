b18fb45570d683f82375a1d40bf6b1cb8183a4c8
Kari Lavikka <kari.lavikka@helsinki.fi>
2021-11-26 14:54:22 +0200

    refactor: typings of `shallowArrayEquals`

--- packages/core/src/utils/arrayUtils.js
+++ packages/core/src/utils/arrayUtils.js
@@ -1,14 +1,20 @@
 /**
- *
+ * @param {T[]} a
+ * @param {T[]} b
+ * @template T
+ */
+export function shallowArrayEquals(a, b) {
+    return a.length == b.length && a.every((s, i) => a[i] === b[i]);
+}
+
+/**
  * @param {A[]} a
  * @param {B[]} b
- * @param {function(A):T} [aAccessor]
- * @param {function(B):T} [bAccessor]
+ * @param {function(A):T} aAccessor
+ * @param {function(B):T} bAccessor
  * @template A, B, T
  */
-export function shallowArrayEquals(a, b, aAccessor, bAccessor) {
-    aAccessor = aAccessor || ((x) => x);
-    bAccessor = bAccessor || ((x) => x);
+export function shallowArrayEqualsWithAccessors(a, b, aAccessor, bAccessor) {
     return (
         a.length == b.length &&
         a.every((s, i) => aAccessor(a[i]) === bAccessor(b[i]))
