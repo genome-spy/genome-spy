2e1fc253695b6bbdc90cadb798d42cb82099c2cb
Kari Lavikka <kari.lavikka@helsinki.fi>
2021-11-27 16:49:08 +0200

    chore: add constructor to `InternMap/Set` typings

--- packages/core/src/types/internmap.d.ts
+++ packages/core/src/types/internmap.d.ts
@@ -4,10 +4,19 @@ declare module "internmap" {
     /**
      * The InternMap class extends the native JavaScript Map class, allowing Dates and other non-primitive keys by bypassing the SameValueZero algorithm when determining key equality.
      */
-    export class InternMap<K = any, V = any> extends Map<K, V> {}
+    export class InternMap<K = any, V = any> extends Map<K, V> {
+        constructor(
+            entries?: readonly (readonly [K, V])[] | null,
+            keyFunction?: KeyFunction
+        );
+    }
 
     /**
      * The InternSet class extends the native JavaScript Set class, allowing Dates and other non-primitive keys by bypassing the SameValueZero algorithm when determining key equality.
      */
-    export class InternSet<T = any> extends Set<T> {}
+    export class InternSet<T = any> extends Set<T> {
+        constructor(values?: readonly T[] | null, keyFunction?: KeyFunction);
+    }
 }
+
+type KeyFunction = (key: any) => string | number | bigint | boolean | symbol;
