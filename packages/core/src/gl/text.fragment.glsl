36365b31425179e774e07d7d196ec24cef466ed8
Kari Lavikka <kari.lavikka@helsinki.fi>
2023-04-24 19:20:03 +0300

    fix(core): eliminate blurry edges in stretched sequence logo letters

--- packages/core/src/gl/text.fragment.glsl
+++ packages/core/src/gl/text.fragment.glsl
@@ -1,4 +1,5 @@
 uniform sampler2D uTexture;
+uniform bool uLogoLetter;
 
 in vec2 vTexCoord;
 in float vEdgeFadeOpacity;
@@ -18,7 +19,15 @@ void main() {
     vec3 c = texture(uTexture, vTexCoord).rgb;
 
     float sigDist = 1.0 - median(c.r, c.g, c.b);
-    float opa = clamp((sigDist - 0.5) * vSlope + 0.5, 0.0, 1.0);
+
+    // Using screen-space derivatives for logo letters because skewed aspect ratios
+    // result in blurry edges otherwise. However, use of screen-space derivatives
+    // results in crappy looking text with regular letters text.
+    float slope = uLogoLetter
+        ? 0.7 / length(vec2(dFdy(sigDist), dFdx(sigDist)))
+        : vSlope;
+
+    float opa = clamp((sigDist - 0.5) * slope + 0.5, 0.0, 1.0);
 
     // Raise to the power of 2.2 to do some cheap gamma correction
     opa *= pow(clamp(vEdgeFadeOpacity, 0.0, 1.0), 2.2);
