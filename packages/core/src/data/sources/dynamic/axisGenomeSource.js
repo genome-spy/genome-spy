aecc01d4719eca08fd5f8726a89089737913f7ce
Kari Lavikka <kari.lavikka@helsinki.fi>
2023-05-12 17:00:34 +0300

    refactor: use `SingleAxisDynamicSource` in `AxisGenomeSource`

--- packages/core/src/data/sources/dynamic/axisGenomeSource.js
+++ packages/core/src/data/sources/dynamic/axisGenomeSource.js
@@ -1,45 +1,19 @@
-import DataSource from "../dataSource";
+import SingleAxisDynamicSource from "./singleAxisDynamicSource";
 
 /**
  * Propagates the genome (chromosome) data associated with the channel.
  * Can be used to generate a genome axis or fancy background grid.
  */
-export default class AxisGenomeSource extends DataSource {
+export default class AxisGenomeSource extends SingleAxisDynamicSource {
     /**
-     * @param {{channel: "x" | "y"}} params
+     * @param {import("../../../spec/data").AxisGenomeData} params
      * @param {import("../../../view/view").default} view
      */
     constructor(params, view) {
-        super();
-
-        this.params = params;
-        this.view = view;
-
-        this.channel = this.params.channel;
-        if (this.channel !== "x" && this.channel !== "y") {
-            throw new Error(
-                `Invalid channel: ${this.channel}. Must be "x" or "y"`
-            );
-        }
-
-        this.scaleResolution = this.view.getScaleResolution(this.channel);
-        if (!this.scaleResolution) {
-            throw new Error(
-                `No scale resolution found for channel "${this.channel}".`
-            );
-        }
+        super(view, params.channel);
     }
 
     async load() {
-        const chroms = this.scaleResolution.getGenome().chromosomes;
-
-        this.reset();
-        this.beginBatch({ type: "file" });
-
-        for (const chrom of chroms) {
-            this._propagate(chrom);
-        }
-
-        this.complete();
+        this.publishData(this.genome.chromosomes);
     }
 }
