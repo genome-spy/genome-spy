f550da6c440f2eaeb6ffc8ac265caf16715ce54d
Kari Lavikka <kari.lavikka@helsinki.fi>
2023-05-12 17:12:19 +0300

    fix: axisTickSource lacked dummy initialization

--- packages/core/src/data/sources/dynamic/axisTickSource.js
+++ packages/core/src/data/sources/dynamic/axisTickSource.js
@@ -79,6 +79,7 @@ export default class AxisTickSource extends SingleAxisDynamicSource {
     }
 
     async load() {
+        this.publishData([]); // TODO: Figure out why this is needed.
         this.onDomainChanged();
     }
 }
