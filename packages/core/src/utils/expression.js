183787f24d06c3af8c2b09d3454b8f3a0ca49d57
Kari Lavikka <kari.lavikka@helsinki.fi>
2023-12-19 20:36:04 +0200

    feat: more functions for use in expressions
    isNumber, isValide, lerp, linearstep, etc.

--- packages/core/src/utils/expression.js
+++ packages/core/src/utils/expression.js
@@ -1,4 +1,59 @@
-import { parseExpression, codegenExpression } from "vega-expression";
+import { parseExpression, codegenExpression, functions } from "vega-expression";
+import {
+    isArray,
+    isBoolean,
+    isNumber,
+    isObject,
+    isRegExp,
+    isString,
+    lerp,
+} from "vega-util";
+import smoothstep from "./smoothstep.js";
+import clamp from "./clamp.js";
+import linearstep from "./linearstep.js";
+
+/**
+ * Some bits are adapted from https://github.com/vega/vega/blob/main/packages/vega-functions/src/codegen.js
+ */
+const functionContext = {
+    clamp,
+    isArray,
+    isBoolean,
+    isDefined(/** @type {any} */ _) {
+        return _ !== undefined;
+    },
+    isNumber,
+    isObject,
+    isRegExp,
+    isString,
+    isValid(/** @type {any} */ _) {
+        // eslint-disable-next-line no-self-compare
+        return _ != null && _ === _;
+    },
+    lerp,
+    linearstep,
+    smoothstep,
+};
+
+/**
+ * @param {typeof codegenExpression} codegen
+ */
+function buildFunctions(codegen) {
+    const fn = functions(codegen);
+    // eslint-disable-next-line guard-for-in
+    for (const name in functionContext) {
+        fn[name] = `this.${name}`;
+    }
+    return fn;
+}
+
+const cg = codegenExpression({
+    forbidden: [],
+    allowed: ["datum"],
+    globalvar: "globalObject",
+    fieldvar: "datum",
+    functions: buildFunctions,
+});
 
 /**
  * @typedef { object } ExpressionProps
@@ -11,13 +66,6 @@ import { parseExpression, codegenExpression } from "vega-expression";
  * @param {string} expr
  */
 export default function createFunction(expr, globalObject = {}) {
-    const cg = codegenExpression({
-        forbidden: [],
-        allowed: ["datum"],
-        globalvar: "globalObject",
-        fieldvar: "datum",
-    });
-
     try {
         const parsed = parseExpression(expr);
         const generatedCode = cg(parsed);
@@ -27,7 +75,7 @@ export default function createFunction(expr, globalObject = {}) {
             "datum",
             "globalObject",
             `"use strict"; return (${generatedCode.code});`
-        );
+        ).bind(functionContext);
 
         /** @type { ExpressionFunction } */
         const exprFunction = /** @param {object} x */ (x) =>
