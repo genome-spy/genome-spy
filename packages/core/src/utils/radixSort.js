9b724387e9a51ccd29327350fee6c1c6b0a8e029
Kari Lavikka <kari.lavikka@helsinki.fi>
2024-01-04 15:10:25 +0200

    chore: radix sort WIP

---
+++ packages/core/src/utils/radixSort.js
@@ -0,0 +1,69 @@
+const MAX_INTEGER = 2147483647;
+const MAX_INTEGER_DIGIT = getDigits([MAX_INTEGER]);
+
+// TODO: Optimize more! Some ideas: https://travisdowns.github.io/blog/2019/05/22/sorting.html
+
+/**
+ * @param {number[]} arr An array of unsigned integers
+ */
+export default function radixSort(arr) {
+    const maxDigits = getDigits(arr);
+
+    let buffer = new Array(arr.length);
+    let bufferPtr = buffer;
+
+    for (let digitIndex = 0; digitIndex < maxDigits; digitIndex++) {
+        let counts = new Array(16).fill(0);
+        const shift = digitIndex * 4;
+        const pow = Math.pow(16, digitIndex);
+
+        /**
+         * @param {*} i number
+         */
+        // eslint-disable-next-line no-loop-func
+        const getDigit = (/** @type {number} */ i) => {
+            const value = arr[i];
+
+            // Need hacks for large numbers because JS bitwise operators only work
+            // with 32-bit integers.
+            // TODO: This could be implemented in WASM for better performance as
+            // it would be able to use 64-bit integers.
+            if (digitIndex >= MAX_INTEGER_DIGIT) {
+                return Math.floor(value / pow) % 16;
+            } else {
+                return (value >> shift) & 0xf;
+            }
+        };
+
+        // Count occurrences of each digit
+        for (let i = 0, n = arr.length; i < n; i++) {
+            counts[getDigit(i)]++;
+        }
+
+        // Prefix sum to get starting indexes
+        for (let i = 1; i < 16; i++) {
+            counts[i] += counts[i - 1];
+        }
+
+        // Sort based on current digit
+        for (let i = arr.length - 1; i >= 0; i--) {
+            bufferPtr[--counts[getDigit(i)]] = arr[i];
+        }
+
+        // Swap buffer and arr for next iteration
+        [arr, bufferPtr] = [bufferPtr, arr];
+    }
+
+    return arr;
+}
+
+/**
+ * @param {number[]} arr An array of unsigned integers
+ */
+function getDigits(arr) {
+    let max = 0;
+    for (let i = 0, n = arr.length; i < n; i++) {
+        max = Math.max(max, arr[i]);
+    }
+    return Math.floor(Math.log2(max) / 4) + 1;
+}
---
+++ packages/core/src/utils/radixSort.test.js
@@ -0,0 +1,39 @@
+import { expect, test } from "vitest";
+import radixSort from "./radixSort.js";
+
+/**
+ * Checks that numbers in an array are in ascending order.
+
+ * @param {number[]} arr An array of unsigned integers
+ */
+function isSorted(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i - 1] > arr[i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+/**
+ * Generates a random array of unsigned integers.
+ *
+ * @param {number} length
+ */
+function generateArray(length) {
+    const arr = new Array(length);
+
+    for (let i = 0; i < length; i++) {
+        arr[i] = Math.floor(Math.random() * 1_000_000_000);
+    }
+
+    return arr;
+}
+
+test("Radix Sort correctly sorts numbers", () => {
+    expect(isSorted(radixSort([1, 2, 3]))).toBeTruthy();
+    expect(isSorted(radixSort([3, 2, 1]))).toBeTruthy();
+    expect(isSorted(radixSort([123, 1234567, 12, 1, 1234]))).toBeTruthy();
+    expect(isSorted(radixSort(generateArray(20_000_000)))).toBeTruthy();
+});
