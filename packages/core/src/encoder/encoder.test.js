289d712a117fc9ac2cc3acabb84e040f5ea49f14
Kari Lavikka <kari.lavikka@helsinki.fi>
2025-06-16 12:45:14 +0300

    chore: fix broken import in test

--- packages/core/src/encoder/encoder.test.js
+++ packages/core/src/encoder/encoder.test.js
@@ -1,4 +1,4 @@
-import { describe, expect, test, skip } from "vitest";
+import { describe, expect, test } from "vitest";
 
 import { createAccessor, createConditionalAccessors } from "./accessor.js";
 import ParamMediator from "../view/paramMediator.js";
