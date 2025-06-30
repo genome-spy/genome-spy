1f8a467730db7472d68f04ff16e6b2396fe7aa50
Kari Lavikka <kari.lavikka@helsinki.fi>
2025-06-30 13:05:00 +0300

    fix(core): improve rendering of small text
    Uses super sampling.
    
    Closes #288

--- packages/core/src/marks/text.fragment.glsl
+++ packages/core/src/marks/text.fragment.glsl
@@ -12,23 +12,49 @@ float median(float r, float g, float b) {
     return max(min(r, g), min(max(r, g), b));
 }
 
+float getDist(vec2 uv) {
+    vec3 c = texture(uTexture, uv).rgb;
+    return 1.0 - median(c.r, c.g, c.b);
+}
+
+/**
+ * Calculates the super-sampled distance to the edge.
+ * This is used to avoid aliasing when rendering small text,
+ * as mip-mapping cannot be used here.
+ * The distance is averaged over a grid of n x n samples.
+ */
+float getSuperDist(vec2 uv) {
+    vec2 dx = dFdx(uv);
+    vec2 dy = dFdy(uv);
+
+    float n = 2.0;
+
+    float sum = 0.0;
+    for (float x = 0.5; x < n; x++){
+        for (float y = 0.5; y < n; y++) {
+            sum += getDist(
+                uv +
+                x / n * dx +
+                y / n * dy
+            ); 
+        }
+    }
+
+    return sum / (n * n);
+}
+
 void main() {
-    // TODO: Really small text should fall back to normal (non-SDF) texture that can be mip-mapped.
-    // Currently small text has severe aliasing artifacts.
+    float sigDist = getSuperDist(vTexCoord);
 
-    vec3 c = texture(uTexture, vTexCoord).rgb;
-
-    float sigDist = 1.0 - median(c.r, c.g, c.b);
-
-    // Using screen-space derivatives for logo letters because skewed aspect ratios
-    // result in blurry edges otherwise. However, use of screen-space derivatives
-    // results in crappy looking text with regular letters text.
-    float slope = uLogoLetter
-        ? 0.7 / length(vec2(dFdy(sigDist), dFdx(sigDist)))
-        : vSlope;
+    float slope = vSlope;
+    if (uLogoLetter) {
+        // Using screen-space derivatives for logo letters because skewed aspect ratios
+        // result in blurry edges otherwise. However, use of screen-space derivatives
+        // results in crappy looking text with regular letters text.
+        slope = 0.7 / length(vec2(dFdy(sigDist), dFdx(sigDist)));
+    }
 
     float opa = clamp((sigDist - 0.5) * slope + 0.5, 0.0, 1.0);
-
     opa *= clamp(vEdgeFadeOpacity, 0.0, 1.0);
 
     opa = pow(opa, vGamma);
