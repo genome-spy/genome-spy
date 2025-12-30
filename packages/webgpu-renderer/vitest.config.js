38086cd5db758e24afd4a3ffe301153f5609e4a0
Kari Lavikka <kari.lavikka@helsinki.fi>
2025-12-31 01:04:14 +0200

    test: test scale functions using playwright

--- packages/webgpu-renderer/vitest.config.js
+++ packages/webgpu-renderer/vitest.config.js
@@ -3,5 +3,6 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
     test: {
         setupFiles: ["./vitest.setup.js"],
+        exclude: ["**/*.gpu.test.js"],
     },
 });
