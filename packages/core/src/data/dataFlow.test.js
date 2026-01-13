ae2260c61bd331cbe30653803eb5b59d634e3e1e
Kari Lavikka <kari.lavikka@helsinki.fi>
2026-01-13 15:46:17 +0200

    test: cover dataflow host removal

--- packages/core/src/data/dataFlow.test.js
+++ packages/core/src/data/dataFlow.test.js
@@ -1,5 +1,39 @@
-import { describe, test } from "vitest";
+import { describe, expect, test } from "vitest";
+import DataFlow from "./dataFlow.js";
+import DataSource from "./sources/dataSource.js";
+import Collector from "./collector.js";
 
 describe("DataFlow", () => {
-    test.todo("TODO");
+    test("removes hosts and ignores observer callbacks", () => {
+        const flow = new DataFlow();
+
+        const sourceA = new DataSource(/** @type {any} */ ({}));
+        const sourceB = new DataSource(/** @type {any} */ ({}));
+
+        flow.addDataSource(sourceA, "a");
+        flow.addDataSource(sourceB, "b");
+
+        const collector = new Collector();
+        flow.addCollector(collector, "c");
+
+        let called = false;
+        flow.addObserver(() => {
+            called = true;
+        }, "c");
+
+        expect(flow.findDataSourceByKey("a")).toBe(sourceA);
+        expect(flow.findCollectorByKey("c")).toBe(collector);
+
+        flow.removeHosts(["a", "c"]);
+
+        expect(flow.findDataSourceByKey("a")).toBeUndefined();
+        expect(flow.findCollectorByKey("c")).toBeUndefined();
+
+        collector.observers[0](collector);
+
+        expect(called).toBe(false);
+        expect(flow.getDataSourcesForHosts(["b", "missing"])).toEqual([
+            sourceB,
+        ]);
+    });
 });
