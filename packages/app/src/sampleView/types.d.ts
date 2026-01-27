6c89b1384137b972689189dd77b26b2d120d3bd5
Kari Lavikka <kari.lavikka@helsinki.fi>
2026-01-27 11:24:32 +0200

    refactor: stronger typing for AttributeIdentifier

--- packages/app/src/sampleView/types.d.ts
+++ packages/app/src/sampleView/types.d.ts
@@ -1,14 +1,13 @@
+import { ViewAttributeSpecifier } from "./sampleViewTypes.js";
 import { SampleHierarchy } from "./state/sampleState.js";
 import { ChromosomalLocus } from "@genome-spy/core/spec/genome.js";
 
 /**
  * An identifier for an abstract attribute. Allows for retrieving an accessor and information.
- *
- * TODO: Stricter typing
  */
 export interface AttributeIdentifier {
     type: string;
-    specifier?: unknown;
+    specifier?: string | ViewAttributeSpecifier;
 }
 
 export type IntervalPoint = number | ChromosomalLocus;
