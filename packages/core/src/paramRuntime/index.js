974cd8572a7e4ad018aacfec1d490d29942a766e
Kari Lavikka <kari.lavikka@helsinki.fi>
2026-02-09 22:56:44 +0200

    refactor(core): move mediator implementation under paramRuntime

--- packages/core/src/paramRuntime/index.js
+++ packages/core/src/paramRuntime/index.js
@@ -1,5 +1,6 @@
 export { default as GraphRuntime } from "./graphRuntime.js";
 export { default as ParamRuntime } from "./paramRuntime.js";
+export { default as ViewParamRuntime } from "./viewParamRuntime.js";
 export { default as ParamStore } from "./paramStore.js";
 export { default as LifecycleRegistry } from "./lifecycleRegistry.js";
 export { compileExpression } from "./expressionCompiler.js";
