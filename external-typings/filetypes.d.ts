794cbb897d1de967cf9b8ea3a1c1319f5c601db9
Kari Lavikka <kari@karilavikka.fi>
2026-03-12 12:10:53 +0200

    fix(playground): declare generated schema module for typecheck

--- external-typings/filetypes.d.ts
+++ external-typings/filetypes.d.ts
@@ -17,3 +17,8 @@ declare module "*?raw" {
     const content: string;
     export default content;
 }
+
+declare module "@genome-spy/core/schema.json" {
+    const schema: Record<string, any>;
+    export default schema;
+}
